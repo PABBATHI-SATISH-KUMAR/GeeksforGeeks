8610e57237e15b7581792c563cdbcd2ccdf4f307
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-30 19:13:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.py
@@ -0,0 +1,63 @@
+#User function Template for python3
+
+class Solution:
+    def getPairs(self, arr):
+        seen = set()
+        pairs = set()
+        for num in arr:
+            complement = -num
+            if complement in seen:
+                pair = tuple(sorted((num, complement)))
+                pairs.add(pair)
+            seen.add(num)
+        return [list(pair) for pair in sorted(pairs)]
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  # array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+class IntMatrix:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n, m):
+        matrix = []
+        # matrix input
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+
+    def Print(self, arr):
+        for i in arr:
+            for j in i:
+                print(j, end=" ")
+            print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        res = obj.getPairs(arr)
+        if len(res) == 0:
+            print()
+        else:
+            IntMatrix().Print(res)
+
+# } Driver Code Ends
