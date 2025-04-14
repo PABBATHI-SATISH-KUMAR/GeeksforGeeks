eb12023abbf8c7f0b45c5eaedbda658cdae34c3e
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-04-14 09:13:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.py
@@ -0,0 +1,19 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+n = int(input())
+
+# Your code here
+fact = 1
+for i in range(1,n+1):
+    fact *= i
+print(fact)
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
