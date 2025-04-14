5982921022c695ddcb193d4ee5bb53d2cccc0041
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-04-14 08:44:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Write Code to Sw
+a,b=b,a
+
+########### Write your code above ###############
+
+print(a, b)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
