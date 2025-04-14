56391de2645ecd032ecbd022097337ae9bcdaa94
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-04-14 09:05:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find index/find-index.py
@@ -0,0 +1,21 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+
+arr = tuple(map(int, input().split()))
+x = int(input())
+
+########### Write your code below ###############
+# Print the index of x in arr
+print(arr.index(x))
+
+########### Write your code above ###############
+
+
+#{ 
+ # Driver Code Starts.()
+# } Driver Code Ends
