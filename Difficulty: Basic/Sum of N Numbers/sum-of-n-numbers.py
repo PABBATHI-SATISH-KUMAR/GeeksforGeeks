74d47632e076fb36afe9140ed9dd2ef9270b10ab
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-04-14 08:45:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.py
@@ -0,0 +1,19 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+n = int(input())
+# Your code here
+sum = 0
+for i in range(n+1):
+    sum += i
+    
+print(sum)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
