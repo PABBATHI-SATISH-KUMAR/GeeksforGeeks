366fc98042dbf66ba18b8344ac9ca7f8835f3049
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-04-30 23:34:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Square Wall Using Nested Loops/print-square-wall-using-nested-loops.java
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  
+        
+        for (int i = 0; i < n; i++) {  
+            for (int j = 0; j < n; j++) {  
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+
+        sc.close();  
+    }
+}
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
