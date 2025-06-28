f480c109dd16c1282c6e1764178af2d9a586b124
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-06-28 10:31:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,29 +1,7 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().missingNumber(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    int missingNumber(int a[]) {
+    int missingNum(int a[]) {
         // code here
-         int n = a.length + 1;  
+        int n = a.length + 1;  
         long expectedSum = (long)n * (n + 1) / 2;
         long actualSum = 0;
 
@@ -32,6 +10,5 @@ class Solution {
         }
 
         return (int)(expectedSum - actualSum);
-
     }
 }
