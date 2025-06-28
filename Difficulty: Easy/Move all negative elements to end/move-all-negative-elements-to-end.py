ffd0cee0c650cd2ad660cc36fedb8e151af53370
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-06-28 10:35:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.py
+++ Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.py
@@ -11,25 +11,3 @@ class Solution:
                 temp.append(arr[i])
         for i in range(len(arr)):
             arr[i] = temp[i]
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-
-    T = int(input())
-
-    while (T > 0):
-        a = [int(x) for x in input().strip().split()]
-        ob = Solution()
-        ob.segregateElements(a)
-        print(*a)
-
-        T -= 1
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
